6a622f3c35e42cd99950c51c59fc4461e18c0c53
fallen158 <fallen7933@gmail.com>
2018-09-12 00:15:26 +0800

    配置多页面入口，index,cart

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -8,13 +8,9 @@ function resolve (dir) {
   return path.join(__dirname, '..', dir)
 }
 
-
-
 module.exports = {
   context: path.resolve(__dirname, '../'),
-  entry: {
-    app: './src/main.js'
-  },
+  entry: utils.entries(),
   output: {
     path: config.build.assetsRoot,
     filename: '[name].js',
